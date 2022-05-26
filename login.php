254e1289799d6c0c715fe743e05d505d99ca212e
soskanerealka2009 <94052243+soskanerealka2009@users.noreply.github.com>
2022-05-26 21:13:40 +0300

    Update login.php

--- login.php
+++ login.php
@@ -121,9 +121,9 @@ else {
   $l=$_POST['login'];
   $p=md5($_POST['pass']);
 
-  $user = 'u47525';
-  $pass = '1167408';
-  $db = new PDO('mysql:host=localhost;dbname=u47525', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $user = 'u47540';
+  $pass = '2201454';
+  $db = new PDO('mysql:host=localhost;dbname=u47540', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
   $sel = $db->query("SELECT login FROM baza WHERE login = $l");
     foreach($sel as $el)
